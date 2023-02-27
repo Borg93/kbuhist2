8a17138bcd1800582616b606c3f41ac600c72062
Gabriel Borg <gabriel.borg@riksarkivet.se>
2023-02-27 15:54:58 +0100

    added workfiles, WIP

---
+++ tests/test_prepreprocessing_khubist.py
@@ -0,0 +1,98 @@
+## WIP
+
+# TODO: Add given when then tests
+
+test_sent = ['\n', 
+             'Psykologiska gåtor\n',
+             'En rättegång för sextio år sedan.\n',
+             'I.\n',
+             'I en vacker salon på ett litet landtställe nära floden, ett par mil ifrån London, sutto på eftermiddagen, samma dag som Alf och Malkolm skildes åt i Kranach, tvenne fruntimmer.\n', 
+              '— Nej, troligen icke, — svarade Astley förvånad och kännande sina misstankar, som nyss varit helt och hållet försvunna, åter vakna.\n',
+                          '— Gott Godt!\n',
+             "heJ »» HEJ» The     quick brown    fox, Augusta på 	Isa, o c h Isa p å A u g u s t",
+             "--",
+             "XI",
+             "XI -"
+             "XI - s"
+             '— Godt!\n']
+
+
+######################
+
+
+^[\.]{2,5}|[;]\.{2,5}|(\.\s){2,5}|[\.]{4,5}|[;(?!)]\.{2,5}|[;(?!)] \.{2,5}
+
+____
+
+hej jag mår bra ...
+....
+...
+; ...
+? ...
+!...
+..... 
+. . . . .
+. . . - ...
+;... hej hur mår du ;
+____
+
+hej jag mår bra ...
+
+- ...
+################
+
+
+[-—] \.{2,5}|[-—]\.{2,5}
+#########
+—...
+— ...
+- ...
+#######
+—
+
+#########
+^(–\s){2,5}|^(\s–){2,5}|^(—\s){2,5}|^(\s—){2,5}
+#########
+– – hej – –
+— — — hej
+ — — — 
+ — —
+– – –  end och start? remove end and replace start with – – –
+ – – –  end och start? remove end and replace start with – – – 
+
+#########
+
+—hej – –
+—hej
+— 
+—
+— end och start? remove end and replace start with – – –
+—  end och start? remove end and replace start with – – – 
+
+####
+(–\s){2,5}$|(\s–){2,5}$|(—\s){,5}$|(\s—){2,5}$
+###########
+
+– – hej – –
+— — — hej
+ — — — 
+ — —  
+–  
+– hej
+– – –  end och start? remove end and replace start with – – –
+– – –  end och start? remove end and replace start with – –
+– – –  end och start? remove end and replace start with – – – –
+ – – –  end och start? remove end and replace start with – – – 
+
+########
+
+– – hej
+— — — hej
+ 
+ — —  
+–  
+– hej
+– – –  end och start? remove end and replace start with
+– – –  end och start? remove end and replace start with
+– – –  end och start? remove end and replace start with
+ – – –  end och start? remove end and replace start with 
