5d6a719e09bb1cfca87e04ab0e493426c2ec715d
gabriel borg <gabriel.borg@riksarkivet.se>
2023-02-28 10:29:54 +0100

    fixes..

--- llm_khubist.py
+++ llm_khubist.py
@@ -1,6 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForMaskedLM, DataCollatorForLanguageModeling ,TrainingArguments, Trainer
 import torch
 from datasets import load_dataset, Dataset, DatasetDict
+import math
 
 
 model_checkpoint = 'KBLab/bert-base-swedish-cased-new'
@@ -35,18 +36,17 @@ data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probabi
 batch_size = 64
 # Show the training loss with every epoch
 logging_steps = len(tokenized_datasets["train"]) // batch_size
-model_name = model_checkpoint.split("/")[-1]
 
 training_args = TrainingArguments(
-    output_dir=f"{model_name}-finetuned-imdb",
+    output_dir="bert-base-swedish-cased-1800",
     overwrite_output_dir=True,
     evaluation_strategy="epoch",
     learning_rate=2e-5,
     weight_decay=0.01,
     per_device_train_batch_size=batch_size,
     per_device_eval_batch_size=batch_size,
-    push_to_hub=True,
-    fp16=True,
+    push_to_hub=False,
+    fp16=False,
     logging_steps=logging_steps,
 )
 
@@ -58,3 +58,13 @@ trainer = Trainer(
     data_collator=data_collator,
     tokenizer=tokenizer,
 )
+
+if __name__ == "__main__":
+
+
+    eval_results = trainer.evaluate()
+    print(f">>> Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
+
+    # trainer.train()
+    # eval_results = trainer.evaluate()
+    # print(f">>> Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
